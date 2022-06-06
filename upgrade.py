94518ecf5f916b414ddb295d0f90dee9f9650612
cgreydak <cgreydak@cisco.com>
2022-06-06 15:31:43 -0400

    import os module

--- upgrade.py
+++ upgrade.py
@@ -1,4 +1,10 @@
-sudo su
-apt update && apt list upgradable
-sleep 15
-apt update && exit
+import os
+os.system ("apt update && apt list upgradable")
+os.system ("sleep 5")
+os.system ("apt upgrade && exit")
+os.system ("sudo -k")
+print("")
+print("")
+print("the end of the upgrade program")
+print("")
+print("")
