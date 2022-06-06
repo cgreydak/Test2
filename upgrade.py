e7aaf584986dd374e03b36f20e3bf730ba2daf91
cgreydak <cgreydak@cisco.com>
2022-06-06 15:37:52 -0400

    playing with diff 1

--- upgrade.py
+++ upgrade.py
@@ -1,10 +1,10 @@
 import os
 os.system ("apt update && apt list upgradable")
-os.system ("sleep 5")
+os.system ("sleep 6")
 os.system ("apt upgrade && exit")
 os.system ("sudo -k")
-print("")
+print("~")
 print("")
 print("the end of the upgrade program")
 print("")
-print("")
+print("~")
