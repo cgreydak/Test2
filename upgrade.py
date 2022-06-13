3780d19638c4d4ad6636369bac61ddecca9c996e
cgreydak <cgreydak@cisco.com>
2022-06-13 15:14:03 -0400

    major rework

--- upgrade.py
+++ upgrade.py
@@ -1,12 +1,44 @@
 import os
-os.system ("apt update && apt list upgradable")
-os.system ("sleep 6")
-os.system ("apt upgrade && exit")
-os.system ("sudo -k")
-print("")
-print("~")
-print("")
-print("the end of the upgrade program")
-print("")
-print("")
-print("~")
+print("This is Python script to upgrade my Linux Devnet Desktop")
+os.system ("apt update && apt list --upgradable")
+print()
+print("*************************************")
+print("Check the list of upgradable packages")
+print("and decide if you want upgrade or not")
+print("*************************************")
+print()
+print()
+#os.system ("sleep 3")
+print("type y if you want to continue or type n if you want to exit")
+a=input()
+while a!="y" and a!="n":
+	try:
+		print()
+		print("************************************************************")
+		print("Type y if you want to continue or type n if you want to exit")
+		print("or type Ctl+C to exit the program")
+		print("************************************************************")
+		print()
+		a=input()
+	except ValueError:
+		a=input()
+while a=="n":
+	print()
+	print("********************************")
+	print("Linux packages were not upgraded")
+	print("********************************")
+	a="stop"
+while a=="y": 
+	os.system ("apt upgrade && exit")
+	os.system ("sudo -k")
+	print()
+	print("************************")
+	print("The upgrade is completed")
+	print("************************")
+	a="stop"
+
+
+
+	
+	
+			
